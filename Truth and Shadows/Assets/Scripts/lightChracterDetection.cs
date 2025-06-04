8cc19d52d4f858cec9c9db928b9b25fa0f201a57
iExploze <54289660+iExploze@users.noreply.github.com>
2025-06-03 23:43:20 -0400

    qs + fixed the getting stuck on the lights

--- Truth and Shadows/Assets/Scripts/lightChracterDetection.cs
+++ Truth and Shadows/Assets/Scripts/lightChracterDetection.cs
@@ -1,66 +1,86 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using Cinemachine.Examples;
 using UnityEngine;
 
-public class lightChracterDetection : MonoBehaviour, ILightHittable
+// Make sure your CharacterMovement script has a public `bool canMove`
+// so you can enable/disable player input from here.
+public class lightCharacterDetection : MonoBehaviour, ILightHittable
 {
-    // this is the external script responsible for interacting
-    // with the player so that it does not get in light for the shadow player
     private const int bufferSize = 60;
-    private Vector3[] lastSafePositions = new Vector3[bufferSize];
-    private int positionIndex = 0;
-    private int validPositions = 0;
+    private Vector3[] lastLightPositions = new Vector3[bufferSize];
+    private int positionIndex = 0;    // Next write index
+    private int validPositions = 0;   // How many slots have actually been filled
 
     private bool isInLight = false;
-    private int backtrackIndex = -1; // -1 means not currently backtracking
+    private bool isBacktracking = false;
+    private int backtrackIndex = -1;
 
     private CharacterMovement characterMovement;
-    // Start is called before the first frame update
+
     void Start()
     {
         characterMovement = GetComponent<CharacterMovement>();
-        // Initialize buffer with current position
+
+        // Pre‐fill the entire array with the starting position so there’s always
+        // at least one “light” coordinate to backtrack to.
         for (int i = 0; i < bufferSize; i++)
         {
-            lastSafePositions[i] = transform.position;
+            lastLightPositions[i] = transform.position;
         }
+
         validPositions = 1;
+        positionIndex = 1;
     }
+
     void Update()
     {
-        // Only store positions if not in light (i.e., safe)
-        if (isInLight)
+        // 1) If we're currently in light and NOT backtracking, record our position:
+        if (isInLight && !isBacktracking)
         {
-            // Store position in circular buffer
-            lastSafePositions[positionIndex] = transform.position;
+            lastLightPositions[positionIndex] = transform.position;
             positionIndex = (positionIndex + 1) % bufferSize;
-            if (validPositions < bufferSize) validPositions++;
-            backtrackIndex = -1; // Reset backtrack when out of light
+
+            if (validPositions < bufferSize)
+                validPositions++;
         }
-        else 
+
+        // 2) If we’re backtracking, walk back through the saved “light” positions:
+        if (isBacktracking)
         {
-            int latestIndex = (positionIndex - 1 + bufferSize) % bufferSize;
-            transform.position = lastSafePositions[latestIndex];
-            backtrackIndex = latestIndex;
+            if (validPositions > 0)
+            {
+                // Place the player at the saved spot:
+                transform.position = lastLightPositions[backtrackIndex];
+
+                // Move the index one step “backwards” in the circular buffer:
+                backtrackIndex = (backtrackIndex - 1 + bufferSize) % bufferSize;
+            }
         }
     }
 
+    // Called the instant any light first intersects the player’s collider
     public void OnLightEnter(Light lightSource)
     {
+        isInLight = true;
+        isBacktracking = false;
         characterMovement.canMove = true;
-        isInLight = false;
     }
 
-    public void OnLightExit(Light lightSource)
-    {
-        characterMovement.canMove = false;
-        isInLight = false;
-    }
-
+    // Called every frame while the player remains in light
     public void OnLightStay(Light lightSource)
     {
         isInLight = true;
-        //Debug.Log("in light");
+    }
+
+    // Called the moment the player exits the light (i.e., steps into shadow)
+    public void OnLightExit(Light lightSource)
+    {
+        isInLight = false;
+        isBacktracking = true;
+        characterMovement.canMove = false;
+
+        // Start the backtrack from the most‐recent “in‐light” slot:
+        backtrackIndex = (positionIndex - 1 + bufferSize) % bufferSize;
     }
 }
