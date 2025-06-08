12e050bfe51dcafa6c9c341755389e3f51521e4d
iExploze <54289660+iExploze@users.noreply.github.com>
2025-06-08 14:13:36 -0400

    qs

--- Truth and Shadows/Assets/Scripts/squidShadowInteraction.cs
+++ Truth and Shadows/Assets/Scripts/squidShadowInteraction.cs
@@ -1,36 +1,54 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class squidShadowInteraction : MonoBehaviour, ILightHittable
 {
     public StateManager playerStateManager;
     public bool isInLight = false;
+
+    private float timeInLight = 0f;
+    private bool hasTransformed = false;
+    private const float transformDelay = 0.01f;
+
     public void OnLightEnter(Light lightSource)
     {
         isInLight = true;
-        playerStateManager.ReturnToNormalForm();
-    }
-
-    public void OnLightExit(Light lightSource)
-    {
-        isInLight = false;
+        timeInLight = 0f;
+        hasTransformed = false;
     }
 
     public void OnLightStay(Light lightSource)
     {
         isInLight = true;
+
+        if (!hasTransformed)
+        {
+            timeInLight += Time.deltaTime;
+            if (timeInLight >= transformDelay)
+            {
+                hasTransformed = true;
+                if (playerStateManager != null)
+                {
+                    playerStateManager.SwitchToHumanForm();
+                }
+                else
+                {
+                    Debug.LogWarning("StateManager not assigned to squidShadowInteraction!");
+                }
+            }
+        }
     }
 
-    // Start is called before the first frame update
+    public void OnLightExit(Light lightSource)
+    {
+        isInLight = false;
+        timeInLight = 0f;
+        hasTransformed = false;
+    }
+
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (playerStateManager == null)
+            playerStateManager = FindObjectOfType<StateManager>();
     }
 }
