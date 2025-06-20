394c7bcb470ad842b495f8d62149f9418cfb090a
Barsamyan-D <113272171+Barsamyan-D@users.noreply.github.com>
2025-06-19 22:25:22 -0400

    Fix sliding window cover code

--- Truth and Shadows/Assets/Scripts/Window/OpenBoxOnKeyPress.cs
+++ Truth and Shadows/Assets/Scripts/Window/OpenBoxOnKeyPress.cs
@@ -2,28 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using UnityEngine.UI;
 
-public class OpenBoxOnKeyPress : MonoBehaviour
+public class SlideWallOnKeyPress : MonoBehaviour
 {
-    public Transform triggerZone; 
+    [Header("Trigger Settings")]
+    public Transform triggerZone;
     public float triggerRadius = 2f;
     public KeyCode interactionKey = KeyCode.F;
-    public float openHeight = 3f;
-    public float openSpeed = 2f;
-    public TextMeshProUGUI promptTMP; 
+    public TextMeshProUGUI promptTMP;
+
+    [Header("Movement Settings")]
+    public Vector3 localMoveDirection = Vector3.right; // default: slide right
+    public float moveDistance = 3f;
+    public float moveSpeed = 2f;
 
     private bool isOpening = false;
     private Vector3 closedPosition;
     private Vector3 openPosition;
+    private bool hasMoved = false;
 
     void Start()
     {
         closedPosition = transform.position;
-        openPosition = closedPosition + Vector3.up * openHeight;
+        openPosition = closedPosition + transform.TransformDirection(localMoveDirection.normalized) * moveDistance;
 
         if (promptTMP != null)
-            promptTMP.gameObject.SetActive(false); // Hide prompt at start
+            promptTMP.gameObject.SetActive(false); // Hide prompt initially
     }
 
     void Update()
@@ -33,23 +37,27 @@ public class OpenBoxOnKeyPress : MonoBehaviour
 
         float distance = Vector3.Distance(player.transform.position, triggerZone.position);
 
-        if (!isOpening && distance < triggerRadius)
+        if (!hasMoved && distance < triggerRadius)
         {
             if (promptTMP != null)
-                promptTMP.gameObject.SetActive(true); // Show prompt
+                promptTMP.gameObject.SetActive(true);
 
             if (Input.GetKeyDown(interactionKey))
+            {
                 isOpening = true;
+                hasMoved = true;
+                if (promptTMP != null)
+                    promptTMP.gameObject.SetActive(false); // Hide after pressing
+            }
         }
-        else
+        else if (!isOpening && promptTMP != null)
         {
-            if (promptTMP != null)
-                promptTMP.gameObject.SetActive(false); // Hide prompt
+            promptTMP.gameObject.SetActive(false);
         }
 
         if (isOpening)
         {
-            transform.position = Vector3.MoveTowards(transform.position, openPosition, openSpeed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, openPosition, moveSpeed * Time.deltaTime);
         }
     }
 }
