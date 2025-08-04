9bb7b14798fb8e48ab741771d6f6700bb19a96a7
DavidLopezSolis <dglopezs@hotmail.com>
2025-08-04 13:01:37 -0400

    Added popup names to the end of the level
    Added popup names to the end of the level

--- Truth and Shadows/Assets/Scripts/EndCredits/NameRevealTrigger.cs
+++ Truth and Shadows/Assets/Scripts/EndCredits/NameRevealTrigger.cs
@@ -4,16 +4,12 @@ using UnityEngine;
 
 public class NameRevealTrigger : MonoBehaviour
 {
-    // The name object we want to reveal
     public GameObject nameToReveal;
 
     private void OnTriggerEnter(Collider other)
     {
-        // Check if the object that entered the trigger is the player
-        // Make sure your player object has the tag "Player"
         if (other.CompareTag("Player"))
         {
-            // Activate the name object, making it visible
             if (nameToReveal != null)
             {
                 nameToReveal.SetActive(true);
